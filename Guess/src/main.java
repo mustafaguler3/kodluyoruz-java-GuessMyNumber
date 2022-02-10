6d27b3f544a504f1c59b7b8a2d01ceddbe2b95f2
mustafaguler3 <mustafa.glr@hotmail.com>
2022-02-10 18:57:21 +0300

    guess my number

---
+++ Guess/src/main.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
+
+        int numberToGuess = random.nextInt(100);
+        int usersGuess;
+        int triesLeft = 5;
+
+        do {
+            System.out.print("You have " + triesLeft + " to guess it correctly. Your guess: ");
+            usersGuess = input.nextInt();
+
+            if (usersGuess < 0 || usersGuess > 100) {
+                System.out.print("It can only be between 0 and 100. ");
+            } else {
+                if (usersGuess > numberToGuess) {
+                    System.out.println("It's lower than " + usersGuess);
+                    triesLeft--;
+                } else if (usersGuess < numberToGuess) {
+                    System.out.println("It's higher than " + usersGuess);
+                    triesLeft--;
+                }
+            }
+        } while (usersGuess != numberToGuess && triesLeft > 0);
+
+
+        if (numberToGuess != usersGuess) {
+            System.out.println("Game Over.");
+        } else {
+            System.out.println("Congrats! It was " + numberToGuess + " indeed!");
+        }
+    }
+}
